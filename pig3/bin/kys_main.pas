c520d9a8f01c12f24730283194827ee16f9a6e2f
sty <scarsty@gmail.com>
2025-07-09 16:03:50 +0800

    run

--- pig3/bin/kys_main.pas
+++ pig3/bin/kys_main.pas
@@ -50,7 +50,6 @@ uses
   machoreader; {needed for reading MACH-O executables}
 
 //程序重要子程
-function SDL_main(argc: integer; argv: ppansichar): integer;
 procedure Run;
 procedure Quit;
 procedure SetMODVersion;
@@ -152,14 +151,6 @@ uses
   Run;
   result := 0;
   end;}
-
-function SDL_main(argc: integer; argv: ppansichar): integer;
-var
-  th: PSDL_Thread;
-begin
-  Run;
-  Result := 0;
-end;
 
 //初始化字体, 音效, 视频, 启动游戏
 procedure Run;
--- pig3/bin/kys_pig3.lpr
+++ pig3/bin/kys_pig3.lpr
@@ -28,7 +28,6 @@ uses
 
   {$IFDEF android}
 exports
-  SDL_main,
   Run,
   SendKeyEvent;
   {$ENDIF}
